0b87cc9baf9f2705834aaa63690d2468b7a2e01a
Heitor Rodrigues Carvalho <heitorrodrigues2909@gmail.com>
2026-07-06 08:48:19 -0300

    add login project

---
+++ mobile-programming/projects/login/lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:login/src/ui/view_models/views/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.red)),
+      home: const MyHomePage(),
+    );
+  }
+}
---
+++ mobile-programming/projects/login/lib/src/ui/view_models/views/home/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 12,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 80),
+                child: Image.asset('assets/images/logo.png', width: 280),
+              ),
+
+              TextField(decoration: const InputDecoration(labelText: 'Email')),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Esqueci a senha'),
+                ),
+              ),
+
+              TextField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Senha'),
+              ),
+
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton(
+                  onPressed: () {},
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 231, 56, 57),
+                  ),
+                  child: const Text('Entrar'),
+                ),
+              ),
+
+              TextButton(onPressed: () {}, child: const Text("Crie uma conta")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
